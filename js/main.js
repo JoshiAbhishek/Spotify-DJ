914a2ae6f15d40fdfb7c2c61d5ec5a27f8c71e2f
Abhishek Joshi <joshi.abhishek@hotmail.com>
2015-11-10 02:28:41 -0800

    update

--- js/main.js
+++ js/main.js
@@ -1,10 +1,85 @@
 var trackQueryData;
 var currentPreview;
-var baseUrl = 'https://api.spotify.com/v1/search?type=track&limit=10&market=US&query=';
-var myApp = angular.module('myApp', []);
 
-var mainCtrl = myApp.controller('mainCtrl', function ($scope, $http) {
+var baseUrl = 'https://api.spotify.com/v1/search?type=track&limit=10&market=US&query=';
+var myApp = angular.module('myApp', ["firebase"]);
+
+var mainCtrl = myApp.controller('mainCtrl', function ($scope, $http, $firebaseArray, $firebaseObject, $firebaseAuth) {
+
+	var ref = new Firebase('https://spotifydj.firebaseio.com/');
+
+	var tracksRef = ref.child('tracks');
+	var usersRef = ref.child('users');
+
+	$scope.users = $firebaseObject(usersRef);
+	$scope.authObj = $firebaseAuth(ref);
+
+	$scope.authData = $scope.authObj.$getAuth();
+
+	/*
+	$scope.authObj.$onAuth(function (authDataOriginal) {
+		$scope.authData = authDataOriginal;
+	});
+
+	*/
+	if ($scope.authData) {
+		$scope.userId = $scope.authData.uid;
+
+		$("#loginButton").css('display', 'none');
+		$("#logoutButton").css('display', 'inline');
+		$("#signupButton").css('display', 'none');
+	}
+	else {
+		$("#loginButton").css('display', 'inline');
+		$("#logoutButton").css('display', 'none');
+	}
+
+	$scope.playlist = $firebaseArray(tracksRef);
+
 	$scope.audioObject = {}
+	$scope.currentTrackAudio = {}
+	
+	//Sign In
+	$scope.signUp = function () {
+
+		$scope.authObj.$createUser({
+			email: $scope.email,
+			password: $scope.password,
+		})
+			.then($scope.logIn)
+
+			.then(function (authData) {
+				$scope.userId = authData.uid;
+				$scope.users.$save()
+			})
+
+			.then($("#signupModal").modal('hide'))
+
+			.catch(function (error) {
+				console.error("Error: ", error);
+			});
+	}
+	
+	//Sign In
+	$scope.signIn = function () {
+		$scope.logIn().then(function (authData) {
+			$scope.userId = authData.uid;
+		})
+	}
+	
+	//Log In
+	$scope.logIn = function () {
+		return $scope.authObj.$authWithPassword({
+			email: $scope.email,
+			password: $scope.password
+		})
+	}
+	
+	//Log Out
+	$scope.logOut = function () {
+		$scope.authObj.$unauth()
+		$scope.userId = false
+	}
 	
 	//Get Tracks From Query
 	$scope.searchTracks = function () {
@@ -15,30 +90,109 @@ var mainCtrl = myApp.controller('mainCtrl', function ($scope, $http) {
 	}
 	
 	//Play Track
-	$scope.play = function (song, id) {
+	$scope.preview = function (song, id) {
 		if ($scope.currentSong == song) {
 			$scope.audioObject.pause()
-			
+
 			$("#" + id).html('<i class="fa fa-play-circle"></i>')
-			
+
 			$scope.currentSong = false
 			return
 		}
 		else {
-			if($scope.audioObject.pause != undefined) $scope.audioObject.pause()
+			if ($scope.audioObject.pause != undefined) $scope.audioObject.pause()
 
-			if(currentPreview != id) {
+			if (currentPreview != id) {
 				$("#" + currentPreview).html('<i class="fa fa-play-circle"></i>')
 			}
 
 			$("#" + id).html('<i class="fa fa-stop"></i>')
-			
+
 			currentPreview = id
-			
+
 			$scope.audioObject = new Audio(song);
 			$scope.audioObject.play()
 			$scope.currentSong = song
 		}
+	}
+	
+	//Add Track To Playlist
+	$scope.addTrack = function (id, name, artist, duration, preview_url) {
+		//if ($scope.authData) {
+			$scope.playlist.$add({
+				id: id,
+				name: name,
+				artist: artist,
+				duration: duration,
+				preview_url: preview_url,
+				upvotes: 0
+			})
+
+				.then(function () {
+					console.log("Added " + name + " by " + artist);
+				})
+		//}
+		//else {
+			//alert("Please sign up or log in");
+		//}
+	}
+
+	$scope.previewPlaylistTrack = function (id) {
+		var previewURL = 'https://embed.spotify.com/?uri=spotify:track:' + id;
+
+		var iFrame = $("<iframe width='300' height='300' frameborder='0' allowtransparency='true'></iframe>");
+
+		iFrame.attr('src', previewURL);
+
+		$('#previewPlaylistWrapper').html(iFrame);
+	}
+	
+	//Play Track From Playlist
+	$scope.playTrack = function (id, name, artist, image_url, preview_url) {
+		if ($scope.currentTrackPlaying == song) {
+			$scope.currentTrackAudio.pause()
+
+		}
+		else {
+			if ($scope.audioObject.pause != undefined) $scope.audioObject.pause()
+			$("#" + currentPreview).html('<i class="fa fa-play-circle"></i>')
+
+			$scope.currentTrackAudio = new Audio(preview_url);
+			$scope.currentTrackAudio.play();
+			$scope.currentTrackPlaying = preview_url;
+		}
+	}
+	
+	//Upvote A Track
+	$scope.upvote = function (id) {
+		//if ($scope.authData) {
+			var tempTrackRef = tracksRef.child(id);
+			var queryRef = tempTrackRef.child('upvotes');
+
+			if (queryRef != null) {
+				queryRef.transaction(function (upvoteNumber) {
+					return upvoteNumber + 1;
+				},
+					function (error, committed, snapshot) {
+						if (error) {
+							alert("Upvote transaction failed abnormally!");
+						}
+						else if (!committed) {
+							alert("Upvote transaction aborted!");
+						}
+						else {
+							console.log("Upvote transaction committed");
+						}
+					}
+					);
+			}
+			else {
+				alert("Accessing Upvote Ref Failed!");
+			}
+		//}
+		//else {
+		//	alert("Please sign up or log in");
+		//}
 	}
 
 });
@@ -46,50 +200,3 @@ var mainCtrl = myApp.controller('mainCtrl', function ($scope, $http) {
 $('body').tooltip({
 	selector: '[title]'
 });
-
-//OLDER
-
-/*
-
-$("#queryButton").on('click', function (e) {
-	e.preventDefault();
-
-	getResults();
-});
-
-var getResults = function () {
-	var data;
-
-	var query = $("#queryInput").val();
-	$.ajax({
-		url: 'https://api.spotify.com/v1/search',
-		type: "GET",
-		dataType: "json",
-		data: {
-			q: query,
-			type: 'track',
-			market: 'US',
-			limit: '10'
-		},
-		success: function (data1) {
-			data = data1;
-			loadResults(data);
-		}
-	});
-};
-
-var loadResults = function (data) {
-	console.log(data);
-
-	var result = JSON.parse(data);
-
-	var tracks = result.tracks;
-	var items = tracks.items;
-
-	$.each(items, function (key, value) {
-		console.log(value.name);
-	});
-
-};
-
-*/
